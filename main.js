807b443829911241b98408eaa292a08f513ea06e
Lauramars <lauramars85@gmail.com>
2020-10-26 14:08:10 +0100

    Logo Animation started

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-// ICONS ANIMATION
+// LOGO ANIMATION
+var logoAni = new TimelineMax({
+  duration: 2,
+});
+function play() {
+  TweenLite.set('#logo', { x: -700 });
+  var tl = new TimelineLite({ delay: 0.5 });
+  tl.to('#logo', 0.5, { y: -300 })
+    .to('#logo', 1.25, { y: 0, ease: Bounce.easeOut })
+    .to('#logo', 2.5, { x: '+=700' }, '-=1.75');
+}
+play();
+// logoAni
 
+//   .to('#logo', 0.1, {
+//     transformOrigin: '50% 100%',
+//     scaleY: 0.25,
+//     yoyo: true,
+//     repeat: 1,
+//   })
+
+//   .to('#logo', 0.75, {
+//     x: 100,
+//     y: -200,
+//     ease: Circ.easeOut,
+//     yoyo: true,
+//     repeat: 1,
+//   });
+
+// ICONS ANIMATION
 var icoAni = new TimelineMax({
   duration: 1,
   repeat: -1,
@@ -32,9 +60,10 @@ icoAni.to(
 );
 
 window.addEventListener('load', function () {
+  logoAni.restart();
   icoAni.restart();
 });
-
+logoAni.restart();
 // $(document).ready(function () {
 //   var scroll_pos = 0;
 //   $('#section').scroll(function () {
